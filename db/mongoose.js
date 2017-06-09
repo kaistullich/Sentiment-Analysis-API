e35e79bca811e4714d53653ca7b174557fdb71ee
kaistullich <kairstullich@gmail.com>
2017-06-09 13:26:31 -0700

    changed DB name in db/mongoose.js

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // use promises with mongoose, instead of callbacks
 mongoose.Promise = global.Promise;
 // connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/SentimentWords');
 
 // export mongoose
 module.exports = {mongoose};
