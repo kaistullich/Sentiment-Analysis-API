b213cc378d3518625728f13ce224a1e5a310a3ab
kaistullich <kairstullich@gmail.com>
2017-06-09 13:26:11 -0700

    imported bodyParser and used it for MW, created POST route for add

--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
 const express = require('express');
+let bodyParser = require('body-parser');
 
 const {addWord, getAllWords} = require("./routesCallbacks");
 
-
+// init app
 let app = express();
 
+// parse application/json
+app.use(bodyParser.json());
+
 // All routes
-app.get('api//add/:word/:score?', addWord);
-app.get('/api/all', getAllWords);
+app.post('/api/v1/add?', addWord);
+app.get('/api/v1/all', getAllWords);
+
 // Start server
 const port = 5000;
 app.listen(port, () => {
