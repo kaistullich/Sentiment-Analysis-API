7c61ae98536adb5986ab507588d83794c36f53a0
kaistullich <kairstullich@gmail.com>
2017-06-09 18:44:19 -0700

    created callback getOneWord

--- server/routesCallbacks.js
+++ server/routesCallbacks.js
@@ -4,9 +4,75 @@ const {statuses} = require('./httpStatusCodes');
 // Local modules/packages
 const {Words} = require('./models/wordsModel');
 const {mongoose} = require('../db/mongoose');
+const ObjectId = require('mongoose').Types.ObjectId;
 
 
-// POST route `api/v1/add?
+// ** GET ROUTES **
+
+// route `api/v1/all`
+function getAllWords(request, response) {
+
+   let reply;
+   // query DB
+   Words.find({}, (error, wordDocs) => {
+      if (error) {
+         throw error
+      }
+      // Build message to send to client
+      reply = {
+         "data": [
+            {
+               "status": statuses(200),
+               "words": wordDocs
+            }
+         ]
+      };
+      // Send response to client
+      response.send(reply);
+      // Exclude these properties from displaying
+   }).select('-__v -_id');
+}
+
+// route `/api/v1/one-word/:word
+function getOneWord(request, response) {
+
+   let reply;
+   let data = request.params.word;
+   // query DB for :word
+   Words.findOne({word: data}).exec((error, wordDoc) => {
+      if (error) {
+         throw error
+      }
+      if (wordDoc === null) {
+         reply = {
+            "data": [
+               {
+                  "status": statuses(200),
+                  "msg": "That word is not in the collection, try again."
+               }
+            ]
+         };
+         response.send(reply);
+      }
+      else {
+         reply = {
+            "data": [
+               {
+                  "status": statuses(200),
+                  "word": wordDoc.word,
+                  "score": wordDoc.score
+               }
+            ]
+         };
+         response.send(reply);
+      }
+   });
+}
+
+
+// ** POST ROUTES **
+
+// route `api/v1/add?
 function addWord(request, response, next) {
    let data = request.body;
    let receivedWord = data.word;
@@ -33,46 +99,25 @@ function addWord(request, response, next) {
          ]
       };
    }
+   // create new instance of `Words` model
    let newWord = new Words({
       word: receivedWord,
       score: receivedScore
    });
-
+   // save the instance to DB
    newWord.save().then((doc) => {
-      // Send response to client
+      // send response to client
       response.send(reply);
       next();
+      // if an error occurred on save to DB
    }, (error) => {
       response.send(error)
    });
 }
 
-// GET route `api/v1/all`
-function getAllWords(request, response) {
-
-   let reply;
-   // query DB
-   Words.find({}, (error, wordDocs) => {
-      if (error) {
-         throw error
-      }
-      // Build message to send to client
-      reply = {
-         "data": [
-            {
-               "status": statuses(200),
-               "words": wordDocs
-            }
-         ]
-      };
-      // Send response to client
-      response.send(reply);
-      // Exclude these properties from displaying
-   }).select('-__v -_id');
-}
-
 // Exports
 module.exports = {
    addWord,
-   getAllWords
+   getAllWords,
+   getOneWord
 };
