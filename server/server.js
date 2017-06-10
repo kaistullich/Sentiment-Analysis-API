ad2080e350ad07d34a611ba071b75e8ef0127cec
kaistullich <kairstullich@gmail.com>
2017-06-09 23:24:12 -0700

    added views, views engine, / route

--- server/server.js
+++ server/server.js
@@ -1,20 +1,39 @@
 const express = require('express');
-let bodyParser = require('body-parser');
+const path = require('path');
+const bodyParser = require('body-parser');
+const hbs = require('handlebars');
 
 const {addWord, getAllWords, getOneWord} = require("./routesCallbacks");
 
 // init app
 let app = express();
-
-// parse application/json
+// set views path
+app.set('views', path.join(__dirname, 'views'));
+// set public path
+app.use(express.static(path.join(__dirname, 'public')));
+// set pug as view engine
+app.set('view engine', 'pug');
+// use bodyParser for middleware
 app.use(bodyParser.json());
 
-// all GET routes
+
+// all HTML routes
+app.get('/', function (req, res) {
+   res.render('index', {
+      title: 'Sentiment API',
+      welcomessage: 'Hello there!'
+   })
+});
+
+
+// all API GET routes
 app.get('/api/v1/all', getAllWords);
 app.get('/api/v1/one-word/:word', getOneWord);
 
-// all POST routes
+
+// all API POST routes
 app.post('/api/v1/add?', addWord);
+
 
 // Start server
 const port = 5000;
