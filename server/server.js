8b3d1b634a93735425eed2f59d2b692d30c6f188
kaistullich <kairstullich@gmail.com>
2017-06-09 18:43:57 -0700

    created /api/v1/one-word/:word

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 let bodyParser = require('body-parser');
 
-const {addWord, getAllWords} = require("./routesCallbacks");
+const {addWord, getAllWords, getOneWord} = require("./routesCallbacks");
 
 // init app
 let app = express();
@@ -9,9 +9,12 @@ let app = express();
 // parse application/json
 app.use(bodyParser.json());
 
-// All routes
-app.post('/api/v1/add?', addWord);
+// all GET routes
 app.get('/api/v1/all', getAllWords);
+app.get('/api/v1/one-word/:word', getOneWord);
+
+// all POST routes
+app.post('/api/v1/add?', addWord);
 
 // Start server
 const port = 5000;
