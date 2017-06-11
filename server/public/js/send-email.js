6a740f82aaa76edd56c07b6777ce3d7a7031d41e
kaistullich <kairstullich@gmail.com>
2017-06-10 17:18:55 -0700

    now sending email input value along with ajax

--- server/public/js/send-email.js
+++ server/public/js/send-email.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+   // success & error divs
    let successAlert = $('#emailSuccess').hide();
    let errorAlert = $('#emailError').hide();
 
@@ -6,28 +7,31 @@ $(document).ready(function () {
       // prevent default form submit action
       event.preventDefault();
 
-      console.log('submit button clicked');
-
       // grab text entered into textarea
       let textareaComment = $('#commentTextarea').val();
+      // grab text entered into email input
+      let emailEntered = $('#emailAddress').val();
 
       // object that will be sent with ajax
       let emailData = {
-         comment: textareaComment
+         comment: textareaComment,
+         emailAddress: emailEntered
       };
 
       // build ajax request
       $.ajax({
-         url: 'http://localhost:5000/process_emai',
+         url: 'http://localhost:5000/process_email',
          data: JSON.stringify(emailData),
          type: 'POST',
          contentType: 'application/json',
          success: (data) => {
             $('#commentTextarea').val('');
+            $('#emailAddress').val('');
             successAlert.show().html('<b>I received your comment, thanks!</b>');
          },
          error: (err) => {
             $('#commentTextarea').val('');
+            $('#emailAddress').val('');
             errorAlert.show().html('<b>An unexpected error occurred :(</b>');
          }
       });
