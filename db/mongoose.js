0149e4c20dc72cf350ce95bb65e690ace68d7e1d
kaistullich <kairstullich@gmail.com>
2017-06-16 16:29:07 -0700

    imported config file

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
+const config = require('../server/config.json');
+
 
 // use promises with mongoose, instead of callbacks
 mongoose.Promise = global.Promise;
+
 // connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/SentimentWords');
+// TODO: uncomment when local DB imported into mLab
+// mongoose.connect(config.dbUri);
 
 // export mongoose
 module.exports = {mongoose};
