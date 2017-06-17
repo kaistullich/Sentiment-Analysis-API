c6a01ccbd49a7ae0a5fa6e5337c12654e646ceb3
kaistullich <kairstullich@gmail.com>
2017-06-17 12:01:43 -0700

    removed local db connection, & added mLab

--- db/mongoose.js
+++ db/mongoose.js
@@ -5,10 +5,8 @@ const config = require('../server/config.json');
 // use promises with mongoose, instead of callbacks
 mongoose.Promise = global.Promise;
 
-// connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/SentimentWords');
-// TODO: uncomment when local DB imported into mLab
-// mongoose.connect(config.dbUri);
+// connect to mLab Collection
+mongoose.connect(config.dbUri);
 
 // export mongoose
 module.exports = {mongoose};
