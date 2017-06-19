4b04335c59987fc2b46ce6816c39f163bf85b838
Kai Stullich <kairstullich@gmail.com>
2017-06-18 17:54:41 -0700

    formatting

--- server/public/js/send-email.js
+++ server/public/js/send-email.js
@@ -1,44 +1,44 @@
 $(document).ready(function () {
-   // success & error divs
-   let successAlert = $('#emailSuccess').hide();
-   let errorAlert = $('#emailError').hide();
+    // success & error divs
+    let successAlert = $('#emailSuccess').hide();
+    let errorAlert = $('#emailError').hide();
 
-   $('form').on('submit', (event) => {
-      // prevent default form submit action
-      event.preventDefault();
+    $('form').on('submit', (event) => {
+        // prevent default form submit action
+        event.preventDefault();
 
-      // grab text entered into textarea
-      let textareaComment = $('#commentTextarea').val();
-      // grab text entered into email input
-      let emailEntered = $('#emailAddress').val();
-      // check if either fields are empty, stop POST method
-      if (textareaComment === '' || emailEntered === '') {
-         errorAlert.show().html('<b>All fields are required</b>');
-         return false;
-      }
+        // grab text entered into textarea
+        let textareaComment = $('#commentTextarea').val();
+        // grab text entered into email input
+        let emailEntered = $('#emailAddress').val();
+        // check if either fields are empty, stop POST method
+        if (textareaComment === '' || emailEntered === '') {
+            errorAlert.show().html('<b>All fields are required</b>');
+            return false;
+        }
 
-      // object that will be sent with ajax
-      let emailData = {
-         comment: textareaComment,
-         emailAddress: emailEntered
-      };
+        // object that will be sent with ajax
+        let emailData = {
+            comment: textareaComment,
+            emailAddress: emailEntered
+        };
 
-      // build ajax request
-      $.ajax({
-         url: 'http://localhost:5000/process_email',
-         data: JSON.stringify(emailData),
-         type: 'POST',
-         contentType: 'application/json',
-         success: (data) => {
-            $('#commentTextarea').val('');
-            $('#emailAddress').val('');
-            successAlert.show().html('<b>I received your comment, thanks!</b>');
-         },
-         error: (err) => {
-            $('#commentTextarea').val('');
-            $('#emailAddress').val('');
-            errorAlert.show().html('<b>An unexpected error occurred :(</b>');
-         }
-      });
-   });
+        // build ajax request
+        $.ajax({
+            url: 'http://localhost:5000/process_email',
+            data: JSON.stringify(emailData),
+            type: 'POST',
+            contentType: 'application/json',
+            success: (data) => {
+                $('#commentTextarea').val('');
+                $('#emailAddress').val('');
+                successAlert.show().html('<b>I received your comment, thanks!</b>');
+            },
+            error: (err) => {
+                $('#commentTextarea').val('');
+                $('#emailAddress').val('');
+                errorAlert.show().html('<b>An unexpected error occurred :(</b>');
+            }
+        });
+    });
 });
