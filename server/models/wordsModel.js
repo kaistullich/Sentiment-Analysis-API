6c5862392f80c7f93ae0a94d9204c00241553a37
Kai Stullich <kairstullich@gmail.com>
2017-06-18 22:21:45 -0700

    created new schema for `files` collection

--- server/models/wordsModel.js
+++ server/models/wordsModel.js
@@ -2,23 +2,39 @@ const mongoose = require('mongoose');
 
 // words schema
 let wordsSchema = mongoose.Schema({
-   word: {
-      type: String,
-      required: true,
-      minlength: 1,
-      trim: true,
-      versionKey: false,
-      unique: true
-   },
-   score: {
-      type: Number,
-      required: true,
-      versionKey: false
-   }
+    word: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true,
+        versionKey: false,
+        unique: true
+    },
+    score: {
+        type: Number,
+        required: true,
+        versionKey: false
+    }
+});
+
+let fileUploadSchema = mongoose.Schema({
+    file: {
+        type: String,
+        required: true,
+        versionKey: false,
+    },
+    email: {
+        type: String,
+        required: true,
+    }
 });
 
 // model for the `wordsSchema`
 let Words = mongoose.model('Word', wordsSchema);
+let UploadFile = mongoose.model('UploadedFiles', fileUploadSchema);
 
 // export Word model
-module.exports = {Words};
+module.exports = {
+    UploadFile,
+    Words,
+};
