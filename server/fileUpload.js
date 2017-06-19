f9a4392c0e4f4c542d13ba82dd8911672b0e0a21
Kai Stullich <kairstullich@gmail.com>
2017-06-18 22:21:20 -0700

    created function for uploading new document to file collection

---
+++ server/fileUpload.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Kai on 6/18/2017.
+ */
+
+const {UploadFile} = require('./models/wordsModel');
+
+function saveFileUpload(file, email) {
+    let newFile = new UploadFile({
+        file: file,
+        email: email
+    });
+
+    newFile.save().then((doc) => {
+        console.log('Successful save!!!');
+    }, (error) => {
+        console.log('ERROR UPLOADING!', error);
+    });
+}
+
+module.exports = {
+    saveFileUpload
+};
